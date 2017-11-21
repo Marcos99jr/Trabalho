b97d18567f3e87f9db9656e3cef0622b9f578763
Marcos99jr <marcos10jrpereira@hotmail.com>
2017-11-21 21:26:55 -0200

    Trabalho, 4° Bimestre, Marcos e Julia

---
+++ js/noticia.js
@@ -0,0 +1,28 @@
+var noticias =[
+{"id":1,
+"titulo":"Better Call Saul",
+"imagem":"https://images.moviepilot.com/images/c_fill,h_295,w_400/t_mp_quality_gif/lbzrfj7skbsgictkcst1/spider-man-an-old-timeless-friend-917606.jpg",
+"descricao":"Oi Senhor",
+"genero":"games"
+},{
+  "id":2,
+  "titulo":"HOMI DAS ARANHA",
+  "imagem":"https://i.makeagif.com/media/9-15-2015/GU4G_b.gif",	
+	"descricao":"Oi Senhor",
+	"genero":"cinema"
+
+},{
+	 "id":3,
+  "titulo":"NOVA HISTORIA",
+  "imagem":"https://i.imgur.com/1zibF2J.gif",	
+	"descricao":"Vamos, tentando entender isso",
+	"genero":"hq"
+	
+},{
+	 "id":4,
+  "titulo":"VINGADORES Guerra infinita",
+  "imagem":"https://www.oficinadanet.com.br/imagens/post/16273/ezgif.com-optimize_1.gif",	
+	"descricao":"Novas imagens vazam de vingadores guerra infinita e publico vai a loucura ",
+	"genero":"hq"
+	
+}]
